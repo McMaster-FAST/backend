89fea7e327233db2c14ec6ed5a079a0909938146
Peter <peterwardell86@gmail.com>
2025-11-18 18:02:21 -0500

    Create temporary endpoint to get all questions

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from .views import QuestionsView
+
 # Import the views from your app's views.py
 from .views import PingView, UploadView, NextTestQuestionView, SubmitTestAnswerView
 
@@ -7,5 +9,6 @@ urlpatterns = [
     path("ping/", PingView.as_view(), name="ping"),
     path("upload/", UploadView.as_view(), name="upload"),
     path("adaptive-test/next-question", NextTestQuestionView.as_view(), name="adaptive_test_next_question"),
-    path("adaptive-test/submit", SubmitTestAnswerView.as_view(), name="adaptive_test_submit")
+    path("adaptive-test/submit", SubmitTestAnswerView.as_view(), name="adaptive_test_submit"),
+    path("questions/", QuestionsView.as_view(), name="question_list"),
 ]
