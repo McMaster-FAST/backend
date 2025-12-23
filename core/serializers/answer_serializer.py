f3208b808b38cca7e8b7326a5a9b8be44467edda
Peter <peterwardell86@gmail.com>
2025-12-23 15:05:48 -0500

    Get basic adaptive test endpoint working

--- core/serializers/answer_serializer.py
+++ core/serializers/answer_serializer.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 
 class AnswerSerializer(serializers.Serializer):
-    user_token = serializers.CharField()
-    question_serial_number = serializers.CharField()
-    selected_answer = serializers.IntegerField()
+    question_id = serializers.CharField(write_only=True)
+    selected_option_id = serializers.CharField(write_only=True)
+
+    correct_option_id = serializers.CharField(read_only=True)
+    explanation = serializers.CharField(read_only=True)
--- core/views/next_test_question.py
+++ core/views/next_test_question.py
@@ -1,17 +1,23 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny
 from rest_framework import status
 
-from ..serializers import NextQuestionSerializer, AdaptiveTestQuestionSerializer, AdaptiveTestQuestionBundle
-from ..models import QuestionGroup, QuestionOption
+from ..serializers import NextQuestionSerializer
+from ..models import Question, QuestionOption, TestSession
+from analytics.models import UserTopicAbilityScore
+from courses.models import UnitSubtopic
 
-class AdaptiveTestError(Exception):
-    pass
+import decimal
+import numpy as np
+
+
+class QuestionBundle:
+    def __init__(self, question, options):
+        self.question = question
+        self.options = options
+
 
 class NextTestQuestionView(APIView):
-    permission_classes = (AllowAny,)
-
     def post(self, request):
         """
         Submits an answer for the adaptive test and gets the next question.
@@ -20,26 +26,46 @@ class NextTestQuestionView(APIView):
         if not serializer.is_valid():
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-        try:
-            # Here you would process the submitted answer and update the adaptive test state
-            # For simplicity, we will just get the next question
-            question, options = get_next_question_data(serializer.validated_data["group_name"])
-            response_serializer = AdaptiveTestQuestionSerializer(AdaptiveTestQuestionBundle(question, options))
-            return Response({"question": response_serializer.data}, status=status.HTTP_200_OK)
-        except AdaptiveTestError as e:
-            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        difficulty_range = decimal.Decimal(5)
+        user = request.user
+
+        subtopic = UnitSubtopic.objects.get(
+            unit__course__code=serializer.validated_data.get("course_code"),
+            unit__name=serializer.validated_data.get("unit_name"),
+            name=serializer.validated_data.get("subtopic_name"),
+        )
         
-def get_next_question_data(question_group_name):
-    try:
-       group_name = QuestionGroup.objects.get(group_name=question_group_name)
-    except QuestionGroup.DoesNotExist:
-        raise AdaptiveTestError(f"Question group with name '{question_group_name}' does not exist.")
-    
-    # TODO: Select question based on adaptive algorithm
-    # The issue is that the existing python library for adaptive testing loads ALL the questions into 
-    # a dataframe and uses that to track the test. We probably can't to that here. Can we repurpose 
-    # question group to temporarily store quesitons that haven't been asked yet.
-    selected_question = group_name.questions.order_by('?').first() 
-    options = QuestionOption.objects.filter(question=selected_question)
-    return selected_question, options
-    
+        test_session, _ = TestSession.objects.get_or_create(
+            user=user, course=subtopic.unit.course, subtopic=subtopic
+        )
+        user_score, _ = UserTopicAbilityScore.objects.get_or_create(
+            user=user, unit_sub_topic=subtopic
+        )
+        theta = user_score.score
+        possible_questions = Question.objects.filter(
+            subtopic=subtopic,
+            difficulty__gte=theta - difficulty_range,
+            difficulty__lte=theta + difficulty_range,
+        )
+        next_question = max(
+            possible_questions,
+            key=lambda q: item_information(
+                q.discrimination, q.difficulty, q.guessing, theta
+            ),
+        )
+        
+        options = QuestionOption.objects.filter(question=next_question)
+        return Response(
+            {
+                "question": NextQuestionSerializer(
+                    QuestionBundle(next_question, options)
+                ).data
+            },
+            status=status.HTTP_200_OK,
+        )
+
+
+def item_information(a, b, c, theta):
+    p = c + (1 - c) / (1 + np.exp(-a * (theta - b)))
+    q = 1 - p
+    return (a**2) * ((q / (p * (1 - c))) * ((p - c) ** 2))
