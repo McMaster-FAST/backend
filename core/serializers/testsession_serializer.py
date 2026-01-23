91cd615f7b385829a81b921b94407cab2c2e1344
Peter Wardell <peterwardell86@gmail.com>
2026-01-22 22:12:03 -0500

    Add TestSession serializers exposing only data we need

--- core/serializers/testsession_serializer.py
+++ core/serializers/testsession_serializer.py
@@ -1,9 +1,31 @@
 from rest_framework import serializers
-from ..models import TestSession
+from core.models import TestSession
+from core.serializers.question_serializer import QuestionSerializer
+from courses.serializers.unit_subtopic_serializer import UnitSubtopicSerializer
+from courses.serializers.course_serializer import CourseSerializer
 
+class QuestionIDSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = QuestionSerializer.Meta.model
+        fields = ['public_id']
 class TestSessionSerializer(serializers.ModelSerializer):
+    # Read-only fields for nested data
+    subtopic_name = serializers.CharField(source="subtopic.name", read_only=True)
+    current_question_id = QuestionIDSerializer(read_only=True)
+    excluded_questions = QuestionIDSerializer(many=True, read_only=True)
+
     class Meta:
         model = TestSession
-        fields = "__all__"
-        # These fields should only be modifiable by the server
-        read_only_fields = ["user", "course", "subtopic", "current_question"]
+        fields = [
+            "subtopic_name",
+            "current_question_id",
+            "excluded_questions",
+            "use_out_of_range_questions",
+        ]
+
+
+class TestSessionWriteSerializer(serializers.ModelSerializer):
+    """Serializer for write operations - only allows specific fields"""
+    class Meta:
+        model = TestSession
+        fields = ["excluded_questions", "use_out_of_range_questions"]
