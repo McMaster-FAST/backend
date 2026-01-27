8b5185df8c9247f53d1f7ad544bec77460b0e5ec
Peter Wardell <peterwardell86@gmail.com>
2026-01-26 20:08:18 -0500

    Migrations and ensure models still have id

--- MacFAST/models.py
+++ MacFAST/models.py
@@ -2,7 +2,7 @@ from django.db import models
 import uuid
 
 class UUIDModel(models.Model):
-    public_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    public_id = models.UUIDField(default=uuid.uuid4, editable=False)
 
     class Meta:
         abstract = True
