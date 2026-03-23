68436cd0448eecc4cc326ffc6f4c6237152de2de
Peter Wardell <peterwardell86@gmail.com>
2026-03-22 21:30:44 -0400

    Require user to be TA or instructor for current course

--- core/serializers/upload_serializer.py
+++ core/serializers/upload_serializer.py
@@ -1,6 +1,8 @@
+from courses.models import Enrolment
 from rest_framework import serializers
-
+from pathlib import Path
 class FileUploadSerializer(serializers.Serializer):
+    supported_formats = ["docx", "xlsx", "csv"]
     file = serializers.FileField(write_only=True)
     course_code = serializers.CharField(write_only=True)
     course_year = serializers.IntegerField(write_only=True)
@@ -8,6 +10,20 @@ class FileUploadSerializer(serializers.Serializer):
     create_required = serializers.BooleanField(default=False, write_only=True)
     
     def validate(self, attrs):
-        # Could add custom validation in the future
+        
+        if Path(attrs["file"].name).suffix.lower() not in [f".{ext}" for ext in self.supported_formats]:
+            raise serializers.ValidationError(
+                f"Unsupported file format. Supported formats are: {', '.join(self.supported_formats)}"
+            )
         return super().validate(attrs)
-    
+
+    def _user_has_permission(self, user):
+        user_enrolment = Enrolment.objects.filter(
+            user=user,
+            course__code=self.context.get("course_code"),
+            course__year=self.context.get("course_year"),
+            course__semester=self.context.get("course_semester"),
+        )
+        if not user_enrolment.exists():
+            return False
+        return user_enrolment.first().is_instructor or user_enrolment.first().is_ta
