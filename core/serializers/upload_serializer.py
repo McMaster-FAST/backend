0a79476582b13ae0e9ab9c5eb2f936187c0aef31
Peter Wardell <peterwardell86@gmail.com>
2026-03-22 21:55:01 -0400

    Fix the serializer

--- core/serializers/upload_serializer.py
+++ core/serializers/upload_serializer.py
@@ -1,6 +1,8 @@
 from courses.models import Enrolment
 from rest_framework import serializers
 from pathlib import Path
+
+
 class FileUploadSerializer(serializers.Serializer):
     supported_formats = ["docx", "xlsx", "csv"]
     file = serializers.FileField(write_only=True)
@@ -8,10 +10,15 @@ class FileUploadSerializer(serializers.Serializer):
     course_year = serializers.IntegerField(write_only=True)
     course_semester = serializers.CharField(write_only=True)
     create_required = serializers.BooleanField(default=False, write_only=True)
-    
+
     def validate(self, attrs):
-        
-        if Path(attrs["file"].name).suffix.lower() not in [f".{ext}" for ext in self.supported_formats]:
+        if not self._user_has_permission(self.context["request"].user):
+            raise serializers.ValidationError(
+                "You do not have permission to upload files for this course."
+            )
+        if Path(attrs["file"].name).suffix.lower() not in [
+            f".{ext}" for ext in self.supported_formats
+        ]:
             raise serializers.ValidationError(
                 f"Unsupported file format. Supported formats are: {', '.join(self.supported_formats)}"
             )
