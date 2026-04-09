3f25a54d71fd0308477d5d00f07408058575ad5e
rajrehman <rajrehman0205@gmail.com>
2026-04-09 04:14:51 -0400

    stats page minor changes

--- core/serializers/adaptive_test/answer_serializer.py
+++ core/serializers/adaptive_test/answer_serializer.py
@@ -3,6 +3,7 @@ from rest_framework import serializers
 class AnswerSerializer(serializers.Serializer):
     question_id = serializers.CharField(write_only=True)
     selected_option_id = serializers.CharField(write_only=True)
+    time_spent = serializers.FloatField(write_only=True, required=False, min_value=0)
 
     correct_option_id = serializers.CharField(read_only=True)
     explanation = serializers.CharField(read_only=True)
